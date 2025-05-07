fa91a123066e8182c043ed8e2757f427c56bfc01
Lisbaldy de Jesus <lisba7474@gmail.com>
2025-05-07 16:51:20 -0300

    Update logging in Button component to include a new feature identifier

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -8,7 +8,7 @@ export const Button: React.FC<ButtonProps> = ({ children, onClick, styles }) => 
   console.log('children: ', children);
   console.log('styles: ', styles);
   console.log('onClick: ', onClick);
-  console.log('children2: ', children);
+  console.log('featUno');
   
   return (
     <button
